92edd924ec0173228fb9a96de1ceca0a632e3479
JHolland <20102340@mail.wit.ie>
2025-09-28 19:51:10 +0100

    refactoring  done

--- app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
@@ -1,27 +1,33 @@
-package ie.setu.placemark
+package ie.setu.placemark.activities
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import ie.setu.placemark.databinding.ActivityPlacemarkBinding
+import ie.setu.placemark.models.PlacemarkModel
+
 import timber.log.Timber
 import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPlacemarkBinding
+    var placemark = PlacemarkModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
-        i("Placemark Activity started..")
+
+        i("Placemark Activity started...")
 
         binding.btnAdd.setOnClickListener() {
-            val placemarkTitle = binding.placemarkTitle.text.toString()
-            if (placemarkTitle.isNotEmpty()) {
-                i("add Button Pressed: $placemarkTitle")
+            placemark.title = binding.placemarkTitle.text.toString()
+            if (placemark.title.isNotEmpty()) {
+                i("add Button Pressed: $placemark.title")
             }
             else {
                 Snackbar
@@ -29,6 +35,5 @@ class PlacemarkActivity : AppCompatActivity() {
                     .show()
             }
         }
-
     }
 }
