1978b79949a1bfb616cc8662838522366f72a587
JHolland <20102340@mail.wit.ie>
2025-10-07 22:17:26 +0100

    exercise 3 done

--- app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
@@ -11,27 +11,27 @@ import timber.log.Timber.i
 class PlacemarkActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPlacemarkBinding
-    private val placemarks = ArrayList<PlacemarkModel>()
+    var placemark = PlacemarkModel()
+    val placemarks = ArrayList<PlacemarkModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         Timber.plant(Timber.DebugTree())
         i("Placemark Activity started...")
 
-        binding.btnAdd.setOnClickListener {
-            val title = binding.placemarkTitle.text.toString()
-            val description = binding.placemarkDescription.text.toString()
-
-            if (title.isNotEmpty()) {
-                val newPlacemark = PlacemarkModel(title, description)
-                placemarks.add(newPlacemark)
-                i("New Placemark Added: $newPlacemark")
-                i("All Placemarks: $placemarks")
-            } else {
-                Snackbar.make(it, "Please Enter a title", Snackbar.LENGTH_LONG).show()
+        binding.btnAdd.setOnClickListener() {
+            placemark.title = binding.placemarkTitle.text.toString()
+            if (placemark.title.isNotEmpty()) {
+                placemarks.add(placemark)
+                i("add Button Pressed: ${placemark}")
+                for (i in placemarks.indices)
+                { i("Placemark[$i]:${this.placemarks[i]}") }
+            }
+            else {
+                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
             }
         }
     }
