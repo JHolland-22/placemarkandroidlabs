647fdb99fcec2456fbcb8ab4716c60709b987a7f
JHolland <20102340@mail.wit.ie>
2025-10-08 18:21:29 +0100

    cancel button  done

--- app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
@@ -3,54 +3,62 @@ package ie.setu.placemark.activities
 import ie.setu.placemark.main.MainApp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
+import ie.setu.placemark.R
 import ie.setu.placemark.databinding.ActivityPlacemarkBinding
 import ie.setu.placemark.models.PlacemarkModel
-import timber.log.Timber
 import timber.log.Timber.i
 
-class PlacemarkActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityPlacemarkBinding
-    var placemark = PlacemarkModel()
-    lateinit var app: MainApp
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityPlacemarkBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        app = application as MainApp
-        i("Placemark Activity started...")
-        binding.btnAdd.setOnClickListener() {
-            placemark.title = binding.placemarkTitle.text.toString()
-            placemark.description = binding.description.text.toString()
-            if (placemark.title.isNotEmpty()) {
-                app.placemarks.add(placemark.copy())
-                i("add Button Pressed: ${placemark}")
-                for (i in app.placemarks.indices)
-                { i("Placemark[$i]:${this.app.placemarks[i]}") }
-            }
-            else {
-                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
-                    .show()
+    class PlacemarkActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityPlacemarkBinding
+        var placemark = PlacemarkModel()
+        lateinit var app: MainApp
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityPlacemarkBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            setSupportActionBar(binding.toolbarAdd)
+
+            app = application as MainApp
+            i("Placemark Activity started...")
+
+            binding.btnAdd.setOnClickListener() {
+                placemark.title = binding.placemarkTitle.text.toString()
+                placemark.description = binding.description.text.toString()
+                if (placemark.title.isNotEmpty()) {
+                    app.placemarks.add(placemark.copy())
+                    i("add Button Pressed: ${placemark}")
+                    for (i in app.placemarks.indices) {
+                        i("Placemark[$i]:${this.app.placemarks[i]}")
+                    }
+                    setResult(RESULT_OK)
+                    finish()
+                } else {
+                    Snackbar.make(it, "Please Enter a title", Snackbar.LENGTH_LONG).show()
+                }
             }
         }
-        binding.btnAdd.setOnClickListener() {
-            placemark.title = binding.placemarkTitle.text.toString()
-            placemark.description = binding.description.text.toString()
-            if (placemark.title.isNotEmpty()) {
-                app.placemarks.add(placemark.copy())
-                i("add Button Pressed: ${placemark}")
-                for (i in app.placemarks.indices) {
-                    i("Placemark[$i]:${this.app.placemarks[i]}")
+
+        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+            menuInflater.inflate(R.menu.menu_placemark_add, menu)
+            return super.onCreateOptionsMenu(menu)
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when (item.itemId) {
+                R.id.item_cancel -> {
+                    setResult(RESULT_CANCELED)
+                    finish()
                 }
-                setResult(RESULT_OK)
-                finish()
             }
-            else {
-                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
-                    .show()
-            }
+            return super.onOptionsItemSelected(item)
         }
     }
-}
+
+
