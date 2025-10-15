496340613e1746c6747adf25c3f8d0d207a9e4d5
JHolland <20102340@mail.wit.ie>
2025-10-15 23:01:54 +0100

    Exercise Solutions and some fixes

--- app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
@@ -1,5 +1,6 @@
 package ie.setu.placemark.activities
 
+import android.R.attr.title
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -9,52 +10,46 @@ import ie.setu.placemark.R
 import ie.setu.placemark.databinding.ActivityPlacemarkBinding
 import ie.setu.placemark.main.MainApp
 import ie.setu.placemark.models.PlacemarkModel
+import timber.log.Timber
 import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityPlacemarkBinding
     var placemark = PlacemarkModel()
-    //val placemarks = ArrayList<PlacemarkModel>()
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        var edit = false
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.toolbarAdd.title = title
         setSupportActionBar(binding.toolbarAdd)
-
         app = application as MainApp
 
-        i("Placemark Activity started...")
-
         if (intent.hasExtra("placemark_edit")) {
-            //Note on minsdk 33+ the following type safe call is
-            /*
-            placemark = intent.getParcelableExtra("placemark_edit",
-                                                   PlacemarkModel::class.java)!!
-             */
+            edit = true
             placemark = intent.extras?.getParcelable("placemark_edit")!!
             binding.placemarkTitle.setText(placemark.title)
             binding.description.setText(placemark.description)
+            binding.btnAdd.setText(R.string.save_placemark)
         }
 
-        binding.btnAdd.setOnClickListener {
+        binding.btnAdd.setOnClickListener() {
             placemark.title = binding.placemarkTitle.text.toString()
             placemark.description = binding.description.text.toString()
-            if (placemark.title!!.isNotEmpty()) {
-                app.placemarks.create(placemark.copy())
-                i("add Button Pressed: $placemark")
-                setResult(RESULT_OK)
-                finish()
-            }
-            else {
-                Snackbar
-                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+            if (placemark.title!!.isEmpty()) {
+                Snackbar.make(it,R.string.enter_placemark_title, Snackbar.LENGTH_LONG)
                     .show()
+            } else {
+                if (edit) {
+                    app.placemarks.update(placemark.copy())
+                } else {
+                    app.placemarks.create(placemark.copy())
+                }
             }
+            setResult(RESULT_OK)
+            finish()
         }
     }
 
@@ -65,9 +60,7 @@ class PlacemarkActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.item_cancel -> {
-                finish()
-            }
+            R.id.item_cancel -> { finish() }
         }
         return super.onOptionsItemSelected(item)
     }
